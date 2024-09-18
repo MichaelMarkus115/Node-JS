9f9288cb0a1d176b08faebb70fa87d22080823de
Michael van den Berg <michaelmarkus115@gmail.com>
2024-09-18 11:19:50 +0200

    commit

--- unit 5/lesson_22/recipe_app/main.js
+++ unit 5/lesson_22/recipe_app/main.js
@@ -1,25 +1,42 @@
 "use strict";
 
 const express = require("express"),
-  layouts = require("express-ejs-layouts"),
   app = express(),
   router = express.Router(),
-  homeController = require("./controllers/homeController"),
-  errorController = require("./controllers/errorController"),
-  subscribersController = require("./controllers/subscribersController.js"),
-  usersController = require("./controllers/usersController.js"),
-  coursesController = require("./controllers/coursesController.js"),
+  layouts = require("express-ejs-layouts"),
   mongoose = require("mongoose"),
-  methodOverride = require("method-override");
+  methodOverride = require("method-override"),
+  errorController = require("./controllers/errorController"),
+  homeController = require("./controllers/homeController"),
+  subscribersController = require("./controllers/subscribersController"),
+  usersController = require("./controllers/usersController"),
+  coursesController = require("./controllers/coursesController"),
+  Subscriber = require("./models/subscriber");
+
+mongoose.Promise = global.Promise;
 
 mongoose.connect(
-  "mongodb://0.0.0.0:27017/confetti_cuisine",
+  "mongodb://localhost:27017/recipe_db",
   { useNewUrlParser: true }
 );
 mongoose.set("useCreateIndex", true);
 
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("Successfully connected to MongoDB using Mongoose!");
+});
+
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
+
+router.use(express.static("public"));
+router.use(layouts);
+router.use(
+  express.urlencoded({
+    extended: false
+  })
+);
 
 router.use(
   methodOverride("_method", {
@@ -27,25 +44,19 @@ router.use(
   })
 );
 
-router.use(layouts);
-router.use(express.static("public"));
-
-router.use(
-  express.urlencoded({
-    extended: false
-  })
-);
 router.use(express.json());
+router.use(homeController.logRequestPaths);
 
 router.get("/", homeController.index);
+router.get("/contact", homeController.getSubscriptionPage);
 
 router.get("/users", usersController.index, usersController.indexView);
 router.get("/users/new", usersController.new);
 router.post("/users/create", usersController.create, usersController.redirectView);
 router.get("/users/:id/edit", usersController.edit);
 router.put("/users/:id/update", usersController.update, usersController.redirectView);
-router.get("/users/:id", usersController.show, usersController.showView);
 router.delete("/users/:id/delete", usersController.delete, usersController.redirectView);
+router.get("/users/:id", usersController.show, usersController.showView);
 
 router.get("/subscribers", subscribersController.index, subscribersController.indexView);
 router.get("/subscribers/new", subscribersController.new);
@@ -60,23 +71,26 @@ router.put(
   subscribersController.update,
   subscribersController.redirectView
 );
-router.get("/subscribers/:id", subscribersController.show, subscribersController.showView);
 router.delete(
   "/subscribers/:id/delete",
   subscribersController.delete,
   subscribersController.redirectView
 );
+router.get("/subscribers/:id", subscribersController.show, subscribersController.showView);
 
 router.get("/courses", coursesController.index, coursesController.indexView);
 router.get("/courses/new", coursesController.new);
 router.post("/courses/create", coursesController.create, coursesController.redirectView);
 router.get("/courses/:id/edit", coursesController.edit);
 router.put("/courses/:id/update", coursesController.update, coursesController.redirectView);
-router.get("/courses/:id", coursesController.show, coursesController.showView);
 router.delete("/courses/:id/delete", coursesController.delete, coursesController.redirectView);
+router.get("/courses/:id", coursesController.show, coursesController.showView);
 
-router.use(errorController.pageNotFoundError);
-router.use(errorController.internalServerError);
+router.post("/subscribe", subscribersController.saveSubscriber);
+
+router.use(errorController.logErrors);
+router.use(errorController.respondNoResourceFound);
+router.use(errorController.respondInternalError);
 
 app.use("/", router);
 
